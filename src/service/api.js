b30aae2e523a9fef7fb92ea5f289f9e6f636f79e
PrincJr <juniorsilvaasafe@gmail.com>
2020-12-03 16:01:54 -0400

    atualizacao url com porta

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 //@TODO recomendo que use um enviroment para url, pois podem mudar me relação a local ou prod
 const api = axios.create({
-  baseURL: 'http://ec2-3-23-113-133.us-east-2.compute.amazonaws.com/api/'
+  baseURL: 'http://ec2-3-23-113-133.us-east-2.compute.amazonaws.com:8080/api/'
 })
 
 export default api
